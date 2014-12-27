2d1c51d63daee06964f7b16448cb886fd46b18f0
Félix Pelletier <felixpelletier23@gmail.com>
2014-12-26 21:37:11 -0500

    Tut4: Colored Cube Needs cleanup

--- experimental/shaders/vertex.glsl
+++ experimental/shaders/vertex.glsl
@@ -1,9 +1,14 @@
 #version 330 core
-in vec3 vertexpos;
+in vec3 vertexPos;
+in vec3 vertexColor;
 uniform mat4 projMat;
 uniform mat4 viewMat;
 
+out vec3 fragmentColor;
+
 void main(){
-	vec4 pos = vec4(vertexpos,1);
+	vec4 pos = vec4(vertexPos,1);
 	gl_Position = projMat * viewMat * pos;
+
+	fragmentColor = vertexColor;
 }
