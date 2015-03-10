a321ad256da14bce11fc702e71f1818c1a3b6f9e
Félix Pelletier <felixpelletier23@gmail.com>
2015-03-10 02:37:31 -0400

    Deleted .build folder

--- .build/soul/Light.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <glm/glm.hpp>
-
-struct Light{
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 direction = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
-};
