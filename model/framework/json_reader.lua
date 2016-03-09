26a681fa5462b012000c489190b5f3631deffd27
Samuel <swwouf@hotmail.com>
2016-03-09 15:06:20 -0500

    Trying out basic json read functionality. Requires cjson (from luarocks)

---
+++ model/framework/json_reader.lua
@@ -0,0 +1,11 @@
+function get(datapath)
+	local json = require('cjson')
+	local file = io.open(datapath.file)
+	local todecode = file:read "*a"
+	local decoded = json.decode(todecode)
+	if datapath.path then
+		return (decoded[datapath.path])
+	end
+	return decoded
+end
+print (get({file='data.json', path='test'}))
